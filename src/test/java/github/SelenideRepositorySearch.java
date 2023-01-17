b26815deb7f101dced692e3daacacb1d563d6003
AlmasN <95028680+Exiliahs@users.noreply.github.com>
2023-01-17 23:42:39 +0600

    final

--- src/test/java/github/SelenideRepositorySearch.java
+++ src/test/java/github/SelenideRepositorySearch.java
@@ -2,6 +2,7 @@ package github;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -11,17 +12,22 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class SelenideRepositorySearch {
 
-    @Test
-    void shouldFindSelenideRepositoryAtTheTop(){
+    @BeforeAll
+    static private void setUp() {
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = false;
         Configuration.headless = false;
+        Configuration.timeout = 5000;
+    }
 
+    @Test
+    void shouldFindSelenideRepositoryAtTheTop(){
         Selenide.open("https://github.com/");
         $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
         $(".repo-list").$(".repo-list-item").$("a").click();
         $("#wiki-tab").click();
-        $("#wiki-body").$("ul").$(byText("Soft assertions")).click();
+        $("#wiki-pages-box").$(withText("Show 2 more pages")).click();
+        $("#wiki-pages-box").$(withText("SoftAssertions")).click();
         $("#wiki-body").$("#user-content-3-using-junit5-extend-test-class").parent().shouldHave(text("JUnit5"));
     }
 }
